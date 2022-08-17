7a91cc45ce0edaf8282282bf40acd70dfd502550
Lina Valand <linavaland@hotmail.co.uk>
2022-08-17 21:18:20 +0100

    main

---
+++ Main.java
@@ -0,0 +1,18 @@
+package Main;
+
+public class Main {
+    int x=5;
+    int y=6;
+
+    public static void main(String[] args) {
+        Main myobj =new Main();
+        Main obj =new Main();
+        System.out.println("myobj.y" + obj.y);
+
+    }
+}
+
+
+
+
+
